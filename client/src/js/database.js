a0a8c5e85485acec55cc44348dce0f016bb337c9
Sarah Leder <Trinity16201@gmail.com>
2024-01-20 14:00:36 -0500

    added event listeners to install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,45 +1,45 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
+export const putDb = async (content) => {
+  console.error("putDb not implemented");
 
-export const putDb = async (content) => {console.error('putDb not implemented');
+  const contactDb = await openDB("jate", 1);
 
-const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction("jate", "readwrite");
 
-const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore("jate");
 
-const store = tx.objectStore('jate');
+  const request = store.add({ id: 1, value: content });
 
-const request = store.add({ id: 1, value: content });
-
-const result = await request;
-console.log('Data saved to the database', result);
+  const result = await request;
+  console.log("Data saved to the database", result);
 };
 
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  console.error("getDb not implemented");
 
-  const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB("jate", 1);
 
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = contactDb.transaction("jate", "readonly");
 
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore("jate");
 
   const request = store.getAll();
 
   const result = await request;
-  console.log('result.value', result);
+  console.log("result.value", result);
   return result;
 };
 
